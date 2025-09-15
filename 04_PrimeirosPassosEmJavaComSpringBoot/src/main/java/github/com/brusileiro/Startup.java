123be66f36b1f717bd6a7561bd8ff538f465589d
brusileiro <brusilribeiro@gmail.com>
2025-09-15 17:16:14 -0300

    Adicionando módulo 04 do curso

--- 04_PrimeirosPassosEmJavaComSpringBoot/src/main/java/github/com/brusileiro/Startup.java
+++ 04_PrimeirosPassosEmJavaComSpringBoot/src/main/java/github/com/brusileiro/Startup.java
@@ -1,4 +1,4 @@
-package github.com.brusileiro;
+package src.main.java.github.com.brusileiro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
