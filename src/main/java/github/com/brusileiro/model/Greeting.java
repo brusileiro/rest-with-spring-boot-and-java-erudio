319cbf2b66c3b20d3e56419d8557b4f6a1e49af6
brusileiro <brusilribeiro@gmail.com>
2025-09-15 16:30:49 -0300

    Meus primeiros passos em java com spring boot

---
+++ src/main/java/github/com/brusileiro/model/Greeting.java
@@ -0,0 +1,4 @@
+package github.com.brusileiro.model;
+
+public record Greeting(long id, String content) {
+}
---
+++ src/test/java/github/com/brusileiro/RestWithSpringBootAndJavaErudioApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.brusileiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAndJavaErudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
